e91af424bdb967f78d6cd6d58ef6c0ef18d31ccf
Macormac <macedoleo238@gmail.com>
2023-06-06 06:21:01 -0600

    Se agrega decorador para sysadmin

---
+++ sysadmin/decorators_sys_admin.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse, HttpResponseServerError
+from django.shortcuts import redirect
+from functools import wraps
+from django.contrib import messages
+
+
+def logged_required(view_func):
+    @wraps(view_func)
+    def wrapped_view(request, *args, **kwargs):
+        # Comprueba si el usuario está autenticado
+        if not request.session.get('logged', False):
+            messages.error(
+                request, 'Necesita autenticarse para acceder al punto deseado')
+            return redirect('login_sys_admin')
+
+        # Ejecuta la vista original
+        response = view_func(request, *args, **kwargs)
+
+        # Asegúrate de que se devuelva una respuesta válida
+        if not isinstance(response, HttpResponse):
+            return HttpResponseServerError('Error interno del servidor')
+
+        return response
+    return wrapped_view
