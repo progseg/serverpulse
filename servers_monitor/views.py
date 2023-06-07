42c42cb24d568c7e48d9257db20ba77e2a9aae53
Macormac <macedoleo238@gmail.com>
2023-06-07 14:22:35 -0600

    Se agrega registro de bitácoras para el monitoreo

--- servers_monitor/views.py
+++ servers_monitor/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+import logging
 from django.http import HttpResponse, HttpRequest, HttpResponseNotAllowed, JsonResponse
 from . import decorators_sys_admin
 from . import models
 
 # Create your views here.
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO,
+                    filename='resgistroInicioAG.log', filemode='a')
+
+
 def get_client_ip(request):
+    logging.info(
+        'get_client_ip Monitoreo: Se hace petición por el método: ' + request.method)
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
@@ -14,26 +22,34 @@ def get_client_ip(request):
 
 
 def monitor(request):
+    logging.info(
+        'monitor Monitoreo: Se hace petición por el método: ' + request.method)
     template = 'monitoreo.html'
     if request.method == 'GET':
         return render(request, template)
 
 
 def state_servidor(request):
-    template='monitoreo.html'
-    d={'list':models.Servidor.objects.all()}
+    logging.info(
+        'Estado del servidor Monitoreo: Se hace petición por el método: ' + request.method)
+    template = 'monitoreo.html'
+    d = {'list': models.Servidor.objects.all()}
     return render(request, template, d)
 
 
 def monitor_cpu(request):
-    template='monitoreo.html'
-    d={'list':models.Servidor.objects.get}
+    logging.info(
+        'monitor_cpu Monitoreo: Se hace petición por el método: ' + request.method)
+    template = 'monitoreo.html'
+    d = {'list': models.Servidor.objects.get}
     pass
 
 
 def on_servidor(request):
-    template='monitoreo.html'
-    d={'list':models.Servidor.objects.all()}
+    logging.info(
+        'Servidor Activo Monitoreo: Se hace petición por el método: ' + request.method)
+    template = 'monitoreo.html'
+    d = {'list': models.Servidor.objects.all()}
     address = get_client_ip(request)
     status = "Activo"
     for server in models.Servidor.objects.all():
@@ -52,6 +68,8 @@ def indeterminate_servidor(server):
 
 
 def off_servidor(request):
+    logging.info(
+        'Servidor Apagado Monitoreo: Se hace petición por el método: ' + request.method)
     address = get_client_ip(request)
     for server in models.Servidor.objects.all():
         if address == server.ipv4_address:
@@ -62,7 +80,9 @@ def off_servidor(request):
 
 
 def recuperar_registros(request):
-    servs =  models.Servidor.objects.all()
+    logging.info(
+        'recupertar_registros AJAX Monitoreo: Se hace petición por el método: ' + request.method)
+    servs = models.Servidor.objects.all()
     result = serializar_registros(servs)
     return JsonResponse(result, safe=False)
 
@@ -70,5 +90,6 @@ def recuperar_registros(request):
 def serializar_registros(servs):
     result = []
     for register in servs:
-        result.append({'Direccion': register.ipv4_address, 'Estado': register.status})
+        result.append({'Direccion': register.ipv4_address,
+                      'Estado': register.status})
     return result
