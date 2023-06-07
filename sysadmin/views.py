073c174fe8c47971e5acbd346a3ee77fc95625f2
Macormac <macedoleo238@gmail.com>
2023-06-07 14:24:02 -0600

    Se agrega registro de bitácoras para el sysadmin

--- sysadmin/views.py
+++ sysadmin/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
+import logging
 from django.http import HttpResponse, HttpRequest, HttpResponseNotAllowed
 from . import decorators_sys_admin
 from django.views.decorators.csrf import csrf_protect
 # Create your views here.
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO,
+                    filename='resgistroInicioAG.log', filemode='a')
 
 
 @decorators_sys_admin.logged_required
 @csrf_protect
 def dashboard_sys_admin(request: HttpRequest) -> HttpResponse:
+    logging.info(
+        'dashboard Sys Admin: Se hace petición por el método: ' + request.method)
     if request.method == 'GET':
         return render(request, 'dashboard.html')
     else:
