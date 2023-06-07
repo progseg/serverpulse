2c82bb25710fc69c276a8d12ab49f554372e47d7
Macormac <macedoleo238@gmail.com>
2023-06-07 14:15:37 -0600

    Se agrega registro de bitácoras para el viewa de admon_global

--- admon_global/views.py
+++ admon_global/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
+import logging
 from django.http import HttpResponse, HttpRequest, HttpResponseNotAllowed
 from . import decorators_admon_global
 from django.views.decorators.csrf import csrf_protect
 # Create your views here.
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO,
+                    filename='resgistroInicioAG.log', filemode='a')
 
 
 @decorators_admon_global.logged_required
 @csrf_protect
 def dashboard_admon_global(request: HttpRequest) -> HttpResponse:
+    logging.info(
+        'dashboard Admin Global: Se hace petición por el método: ' + request.method)
     if request.method == 'GET':
         return render(request, 'dashboard.html')
     else:
