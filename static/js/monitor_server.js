e29de848dad692b43057096091db0683c75927c1
Macormac <macedoleo238@gmail.com>
2023-06-07 09:15:40 -0600

    Se agregar js de monitoreo

---
+++ static/js/monitor_server.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	setInterval(function() {
+		$.get("/recuperar_registros", function(data, status) {
+			if (status == "success"){
+			lista = "";
+		for(var i=0; i< data.length; i++){
+			lista += "<tr>"+"<td>"+data[i].Servidor+"</td>"+"<td>"+data[i].Direccion+"</td>"+"<td>"+data[i].Estado+"</td>"+"<tr>"
+			}
+				$("#cambiar").html(lista);
+			}
+		});
+	}, 1000) ;
+});
