ddf83c542281052dc7d1b97fe9d22ca1f1a0962c
Macormac <macedoleo238@gmail.com>
2023-06-14 08:44:03 -0600

    Se modificaron los formularios para SinginAdmin y SinginServer en forms de admon_global y se le integraron sus respectivas validaciones

--- admon_global/forms.py
+++ admon_global/forms.py
@@ -1,6 +1,8 @@
 from django import forms
-from auth_app.validators import *
+from . import validators
 from auth_app.models import Servidor, Sysadmin
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV2Checkbox
 
 USERNAME_MAX_LEN = 20
 USERNAME_MIN_LEN = 4
@@ -13,102 +15,111 @@ LEN_CHATID = 10
 
 LEN_TOKEN2FA = 24
 
-class SinginAdmin(forms.ModelForm):
-    confirm_password = forms.CharField(
-        label='Contraseña de confirmación',
-        widget=forms.PasswordInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Ingrese de nuevo la contraseña',
-                'id': 'repeat_password',
-                'required': 'required',
-            }
-        ),
-        min_length=PASSWD_MIN_LEN,
-        max_length=PASSWD_MAX_LEN,
+class SinginAdmin(forms.Form):
+    user_name = forms.CharField(
+        label='Nombre Sys Admin ',
+        max_length=USERNAME_MAX_LEN,
+        min_length=USERNAME_MIN_LEN,
+        required=True,
         validators=[
-            contains_digits,
-            contains_lowecase,
-            contains_uppercase,
-            contais_special,
-            contains_spaces
+            validators.validate_username,
+            validators.contains_spaces,
         ]
     )
+    passwd = forms.CharField(
+        label='Contraseña',
+        max_length=PASSWD_MAX_LEN,
+        min_length=PASSWD_MIN_LEN,
+        required=True,
+        widget=forms.PasswordInput,
+        validators=[
+            validators.contains_digits,
+            validators.contains_lowecase,
+            validators.contains_uppercase,
+            validators.contais_special,
+            validators.contains_spaces,
+        ]
+    )
+    repeat_passwd = forms.CharField(
+        label='Repetir contraseña',
+        max_length=PASSWD_MAX_LEN,
+        min_length=PASSWD_MIN_LEN,
+        required=True,
+        widget=forms.PasswordInput,
+        validators=[
+            validators.contains_digits,
+            validators.contains_lowecase,
+            validators.contains_uppercase,
+            validators.contais_special,
+            validators.contains_spaces,
+        ]
+    )
+    chat_id = forms.CharField(
+        label='ID de su chat con su bot en Telegram',
+        max_length=LEN_CHATID,
+        min_length=LEN_CHATID,
+        required=True,
+        validators=[
+            validators.only_digits,
+            validators.contains_spaces,
+        ]
+    )
+    token_bot = forms.CharField(
+        label='Token de su BOT en Telegram',
+        max_length=LEN_TOKEN_BOT,
+        min_length=LEN_TOKEN_BOT,
+        required=True,
+        validators=[
+            validators.telegram_bot_token,
+            validators.contains_spaces,
+        ]
+    )
+    captcha = ReCaptchaField(
+        widget=ReCaptchaV2Checkbox
+    )
 
-    class Meta:
-        model = Sysadmin
-        fields = ('user_name', 'chat_id', 'token_bot', 'passwd')
-        label = {
-            'user_name': 'nombre admin',
-            'passwd': 'Contraseña del servidor',
-            'chat_id': 'chat id de telegram',
-            'token_bot': 'token bot de telegram',
-        }
-        widgets = {
-            'user_name': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'user_name del SysAdmin',
-                }
-            ),
-            'passwd': forms.PasswordInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Contraseña del sysadmin',
-                }
-            ),
-            'chat_id': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Chat ID de telegram',
-                }
-            ),
-            'token_bot': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Token bot de telegram',
-                }
-            ),
-        },
-        # Verificación de contraseña (10char,uppercase,lowecase,digits,special)
 
-
-class SinginServer(forms.ModelForm):
-    confirm_password = forms.CharField(label='Contraseña de confirmación', widget=forms.PasswordInput(
-        attrs={
-            'class': 'form-control',
-            'placeholder': 'Ingrese de nuevo la contraseña',
-            'id': 'repeat_password',
-            'required': 'required',
-        }
-    ))
-
-    class Meta:
-        model = Servidor
-        fields = ('ipv4_address', 'sysadmin', 'password')
-        label = {
-            'ipv4_address': 'IP del servidor',
-            'password': 'Contraseña del servidor',
-            'sysadmin': 'Administrador del servidor',
-        }
-        widgets = {
-            'ipv4_address': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'IP del nuevo servidor',
-                }
-            ),
-            'password': forms.PasswordInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Contraseña del servidor',
-                }
-            ),
-            'sysadmin': forms.Select(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Administrador a monitorear',
-                }
-            ),
-        }
-        # Verificación de contraseña (10char,uppercase,lowecase,digits,special)
+class SinginServer(forms.Form):
+    ipv4_address = forms.CharField(
+        label='Dirección IP del Servidor',
+        required=True,
+        validators=[
+            validators.validate_ipv46_address,
+        ]
+    )
+    sysadmin = forms.CharField(
+        label='Sys Admin asociado al Servidor',
+        required=True,
+        widget=forms.Select,
+    )
+    passwd = forms.CharField(
+        label='Contraseña',
+        max_length=PASSWD_MAX_LEN,
+        min_length=PASSWD_MIN_LEN,
+        required=True,
+        widget=forms.PasswordInput,
+        validators=[
+            validators.contains_digits,
+            validators.contains_lowecase,
+            validators.contains_uppercase,
+            validators.contais_special,
+            validators.contains_spaces,
+        ]
+    )
+    repeat_passwd = forms.CharField(
+        label='Repetir contraseña',
+        max_length=PASSWD_MAX_LEN,
+        min_length=PASSWD_MIN_LEN,
+        required=True,
+        widget=forms.PasswordInput,
+        validators=[
+            validators.contains_digits,
+            validators.contains_lowecase,
+            validators.contains_uppercase,
+            validators.contais_special,
+            validators.contains_spaces,
+        ]
+    )
+    captcha = ReCaptchaField(
+        widget=ReCaptchaV2Checkbox
+    )
